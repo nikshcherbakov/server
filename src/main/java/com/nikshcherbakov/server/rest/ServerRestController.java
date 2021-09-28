46b5cc60c9ea6ea24d75fb9beeda1976aa18d8fe
nikshcherbakov <shcerbackow@yandex.ru>
2021-09-28 17:52:14 +0300

    Initial commit

---
+++ src/main/java/com/nikshcherbakov/server/rest/ServerRestController.java
@@ -0,0 +1,12 @@
+package com.nikshcherbakov.server.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ServerRestController {
+
+
+
+}
