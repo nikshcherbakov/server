05d8aafef0448f64910121f9f3d98590b13e3fbe
nikshcherbakov <shcerbackow@yandex.ru>
2021-09-28 18:15:30 +0300

    Added submodule, implemented rest controller

--- src/main/java/com/nikshcherbakov/server/rest/ServerRestController.java
+++ src/main/java/com/nikshcherbakov/server/rest/ServerRestController.java
@@ -1,12 +1,17 @@
 package com.nikshcherbakov.server.rest;
 
+import com.nikshcherbakov.api.MyApi;
+import com.nikshcherbakov.api.dto.ObjectDto;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
-public class ServerRestController {
+public class ServerRestController implements MyApi {
 
-
+    @Override
+    public ObjectDto getObject() {
+        return new ObjectDto("Example name", "Example value");
+    }
 
 }
